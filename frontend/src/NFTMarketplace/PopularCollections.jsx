861ca544ae8fa24153e46b909b0150d9a17daf54
Yonghao Lin <80102852+TheSkyRS@users.noreply.github.com>
2024-12-03 19:28:26 -0500

    image bug fix

--- frontend/src/NFTMarketplace/PopularCollections.jsx
+++ frontend/src/NFTMarketplace/PopularCollections.jsx
@@ -79,7 +79,19 @@ const PopularCollections = () => {
             const metadata = JSON.parse(data);
             // console.log('Metadata:', metadata);
             // const imageURI = `http://34.72.243.54:8080/ipfs/${metadata.image}`;
-            const imageURI = metadata.image;
+            let imageURI;
+            if (metadata.image) {
+              // const imageURI = `http://34.72.243.54:8080/ipfs/${metadata.image}`;
+              // const imageURI = `https://ipfs.io/ipfs/${metadata.image}`;
+              
+              if (/^http(s)?:\/\//.test(metadata.image)) {
+                imageURI = metadata.image.replace(
+                  /^https?:\/\/ipfs\.io\/ipfs\//,
+                  "http://34.72.243.54:8080/ipfs/"
+              );
+              } else {
+                imageURI = `http://34.72.243.54:8080/ipfs/${metadata.image}`;
+              }}
             // console.log('Image URI:', imageURI);
             tokens.push({
               tokenId,
